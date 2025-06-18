58b27942111826192e5c172a55baf04b111d690f
Aarushi Gupta <aarushigupta47@gmail.com>
2025-06-18 19:10:35 +0530

    jaadu

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Jatin loves Aarushi</p>
+        <p>Jatin love Aarushi</p>
         <a
           className="App-link"
           href="https://reactjs.org"
